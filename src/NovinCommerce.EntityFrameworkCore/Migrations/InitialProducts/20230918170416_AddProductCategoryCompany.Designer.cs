515313d0c2e492cd3a5facdd332179afa9e0f37e
Mohammad Mahdi <MahdiMoghaddam2005@gmail.com>
2023-09-19 07:52:23 +0330

    Add Product Migrations

--- src/NovinCommerce.EntityFrameworkCore/Migrations/InitialProducts/20230918170416_AddProductCategoryCompany.Designer.cs
+++ src/NovinCommerce.EntityFrameworkCore/Migrations/InitialProducts/20230918170416_AddProductCategoryCompany.Designer.cs
@@ -3,18 +3,21 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NovinCommerce.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
 
 #nullable disable
 
-namespace NovinCommerce.Migrations
+namespace NovinCommerce.Migrations.InitialProducts
 {
     [DbContext(typeof(NovinCommerceDbContext))]
-    partial class NovinCommerceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230918170416_AddProductCategoryCompany")]
+    partial class AddProductCategoryCompany
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
